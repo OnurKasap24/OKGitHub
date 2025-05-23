726057bd2f8f44a259b2e5af6d1780d6cc253d61
vdi-terminal <gfdgfdzs@fdfdf>
2025-05-23 12:26:24 +0200

    :)

--- task 2.py
+++ task 2.py
@@ -1,1 +1,12 @@
-print("Arda SAlaq")
+def multiply_two_numbers(a, b):
+    return a * b
+
+
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+
+
+product_result = multiply_two_numbers(a, b)
+print(f"The product of {a} and {b} is {product_result}")
+
+print(":)))))")
